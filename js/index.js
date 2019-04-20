df17fa87d32f36d2538a05a25f531910a06fdee8
但盼风雨来 <31466416@qq.com>
2019-04-21 01:52:14 +0800

    个人博客

---
+++ js/index.js
@@ -0,0 +1,24 @@
+function showpic() {
+  var a = document.getElementById('demo');
+  var b = a.style.display
+  if (b == 'none') {
+      a.style.display = 'block';
+  } else {
+    a.style.display = 'none';
+  }
+}
+
+function showpic2() {
+  var a = document.getElementById('demo2');
+  var b = a.style.display
+  if (b == 'none') {
+      a.style.display = 'block';
+  } else {
+    a.style.display = 'none';
+  }
+}
+
+// window.onload=function over() {
+//   var a = document.getElementById('pic');
+//   alert("大傻逼")；
+// }
